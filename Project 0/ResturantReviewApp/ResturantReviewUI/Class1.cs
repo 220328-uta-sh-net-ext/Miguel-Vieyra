903711683c5d74615a7d09a3d00de1bf870cca30
Miguel Vieyra <miguel-lauro@hotmail.com>
2022-04-25 15:25:31 -0700

    updates to project

--- Project 0/ResturantReviewApp/ResturantReviewUI/Class1.cs
+++ Project 0/ResturantReviewApp/ResturantReviewUI/Class1.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using ResturantReviewUI;
+
+bool repeat = true;
+IMenu menu = new MainMenu();
+
+
+while (true)
+{
+   
+    menu.Display();
+    string ans = menu.UserChoice();
+
+    switch (ans)
+    {
+        case "SearchResturant":
+            Console.WriteLine("SearchResturant() Method is in progress.....");
+            break;
+        case "AddResturant":
+            Console.WriteLine("AddResturant() Method implementation is in progress.....");
+            break ;
+        case "MainMenu":
+            menu=new MainMenu();
+            break;
+        case "Exit":
+            repeat = false;
+            break;
+
+        default:
+            Console.WriteLine("View does not exist");
+            Console.WriteLine("Please press <enter> to continue");
+            Console.ReadLine();
+            break;
+    }
+
+}
---
+++ Project 0/ResturantReviewApp/ResturantReviewUI/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResturantReviewUI
+{
+    class MainMenu : IMenu
+    {
+        public void Display()
+        {
+            Console.WriteLine("Welcome to Resturant App");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("Press <2> to Search Resturant");
+            Console.WriteLine("Press <1> to Add Resturant to your profile");
+            Console.WriteLine("Press <0> to Exit");
+        }
+
+
+        public string UserChoice()
+        {
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "0":
+                    return "Exit";
+                case "1":
+                    return "AddResturant";
+                case "2":
+                    return "SearchResturant";
+                default:
+                    Console.WriteLine("Please input a valid response");
+                    Console.WriteLine("Please press <enter> to continue");
+                    Console.ReadLine();
+                    return "Mainmenu";
+            }
+        }
+    }
+}
